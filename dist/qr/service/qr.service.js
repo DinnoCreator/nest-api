541cb346bc40852b787e4ea65a71dc5f637d3917
DinnoCreator <97753291+DinnoCreator@users.noreply.github.com>
2023-02-07 19:51:30 +0100

    Update qr.service.js

--- dist/qr/service/qr.service.js
+++ dist/qr/service/qr.service.js
@@ -13,7 +13,7 @@ let QrService = class QrService {
     async qrGetter() {
         const feeds = ["feed1", "feed2", "feed3", "feed4"];
         const number = Math.floor(Math.random() * feeds.length);
-        const code = await qrcode.toDataURL(`https://movie-viewer.onrender.com//${feeds[number]}`);
+        const code = await qrcode.toDataURL(`https://movie-viewer.onrender.com/${feeds[number]}`);
         return { qrcode: code };
     }
 };
